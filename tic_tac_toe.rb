42fca74af9008f0a7e4c8ac0f4115e75e2cf7cc9
TurtleNaTree <kmcqueen108121@hotmail.com>
2023-01-28 17:57:25 -0100

    started work on the user input method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -29,7 +29,26 @@ class TicTacToe
         puts "The current player is #{current_player} the game board is #{game_board}}"
     end
 
+    def place_piece
+        spot_selected = false
+        
+        puts "#{current_player} enter a number (1 - 9) to choose a spot."
+
+    end
+    def check_for_horizontal_win
+        x = 0
+        @game_board[2] = "X"
+        @game_board[4] = "O"
+        while x < 7
+            row_to_check = game_board.slice(x,3)
+            puts "current row is #{row_to_check}"
+            x += 3
+        end
+
+    end
 end
 
 lets_play = TicTacToe.new
 lets_play.display_board()
+lets_play.check_for_horizontal_win
+puts [1, 2, 3, 4, 5, 6, 7, 8, 9] - [1, 3, 5, 7, 9]
