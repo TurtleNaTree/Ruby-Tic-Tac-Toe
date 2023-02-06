387f213d76f453fcd156eef00efa40881cd0d348
TurtleNaTree <kmcqueen108121@hotmail.com>
2023-02-06 18:46:10 -0100

    The game can now let players enter a x or o on the board and change the current player

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -21,10 +21,10 @@ class TicTacToe
         end
     end
 
-    def check_game_space(game_space, index)
-        game_space == nil ? index + 1 : game_space
+    def check_player_choice(player_choice)
+        (player_choice > 0) && (player_choice < 10) ? true : false
     end
-    
+
     def get_game_space(game_space, index)
         game_space == nil ? index + 1 : game_space
     end
@@ -33,17 +33,30 @@ class TicTacToe
         puts "The current player is #{current_player} the game board is #{game_board}}"
     end
 
+    def switch_player
+        current_player == "Player 1" ? @current_player = "Player 2" : @current_player = "Player 1"
+    end
+
     def place_piece
         spot_selected = false
         player_choice = ""
         until spot_selected == true
             puts "#{current_player} enter a number (1 - 9) to choose a spot."
-            player_choice = gets.chomp
-            spot_selected = true if player_choice
+            player_choice = gets.chomp.to_i
+            spot_selected = true if check_player_choice(player_choice)
         end
-        game_board[player_choice.to_i - 1] = "x"
 
-        
+        player_choice -= 1
+
+        case current_player
+            when "Player 1"
+                game_board[player_choice] = "x"
+                switch_player
+            when "Player 2"
+                game_board[player_choice] = "0"
+            else
+                puts "Something went wrong in place_piece"
+        end 
 
     end
     def check_for_horizontal_win
@@ -64,4 +77,4 @@ lets_play.display_board()
 lets_play.check_for_horizontal_win
 lets_play.place_piece
 lets_play.display_board()
-puts [1, 2, 3, 4, 5, 6, 7, 8, 9] - [1, 3, 5, 7, 9]
+#puts [1, 2, 3, 4, 5, 6, 7, 8, 9] - [1, 3, 5, 7, 9]
