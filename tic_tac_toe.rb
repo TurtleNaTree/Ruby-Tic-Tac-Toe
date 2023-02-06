2496e1368bc7a91d02c2fd04ff8c0cdd78940b2b
TurtleNaTree <kmcqueen108121@hotmail.com>
2023-02-06 18:10:20 -0100

    Fixed a bug in the display board method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -13,15 +13,19 @@ class TicTacToe
         puts "\t\t#{current_player}'s Turn!"
         puts "__________________________________________________"
         game_board().each_with_index do |game_space, index|
-            unless check_game_space(game_space, index) % 3 == 0
-                print "\t#{check_game_space(game_space, index)} \t |"
+            unless (index + 1) % 3 == 0
+                print "\t#{get_game_space(game_space, index)} \t |"
             else
-                puts "\t#{check_game_space(game_space, index)} \n __________________________________________________"
+                puts "\t#{get_game_space(game_space, index)} \n __________________________________________________"
             end
         end
     end
 
     def check_game_space(game_space, index)
+        game_space == nil ? index + 1 : game_space
+    end
+    
+    def get_game_space(game_space, index)
         game_space == nil ? index + 1 : game_space
     end
 
@@ -31,14 +35,21 @@ class TicTacToe
 
     def place_piece
         spot_selected = false
+        player_choice = ""
+        until spot_selected == true
+            puts "#{current_player} enter a number (1 - 9) to choose a spot."
+            player_choice = gets.chomp
+            spot_selected = true if player_choice
+        end
+        game_board[player_choice.to_i - 1] = "x"
+
         
-        puts "#{current_player} enter a number (1 - 9) to choose a spot."
 
     end
     def check_for_horizontal_win
         x = 0
-        @game_board[2] = "X"
-        @game_board[4] = "O"
+        @game_board[2] = "x"
+        @game_board[4] = "o"
         while x < 7
             row_to_check = game_board.slice(x,3)
             puts "current row is #{row_to_check}"
@@ -51,4 +62,6 @@ end
 lets_play = TicTacToe.new
 lets_play.display_board()
 lets_play.check_for_horizontal_win
+lets_play.place_piece
+lets_play.display_board()
 puts [1, 2, 3, 4, 5, 6, 7, 8, 9] - [1, 3, 5, 7, 9]
